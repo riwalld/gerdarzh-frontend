2fcf1df7652f5138b8110551c27bade36d714632
Kerbetth <kerbetth@protonmail.com>
2024-05-06 17:25:56 +0200

    implement Vue components for wordstems page

---
+++ src/components/Wordstems/WordstemRow.js
@@ -0,0 +1,31 @@
+import { setLanguages } from "../Config/Config.js";
+export default {
+    template: `
+    <tr>
+      <td>
+          <span class="langws" :title="wordstem.wordStemLanguage"> {{ wordstem.wordStemLanguage }} </span>
+      </td>
+      <td> <b><a style="color: rgb(50, 50, 150)" :href="'/lexique/entree?nom=' + wordstem.wordStemName">{{ wordstem.wordStemName }}  </a></b></td>
+      <td>
+          <span class="langws" :title="wordstem.wordClass">{{ wordstem.wordClass }}</span>
+          &nbsp;
+          <span class="langws" :title="wordstem.wordGender">{{ wordstem.wordGender }}</span>
+      </td>
+      <td>
+      <span class="langws" title="français">fr.</span>
+      : <i>{{ wordstem.frTranslation }}</i>
+      <br><span class="langws" title="anglais">ang.</span>
+      : <i>{{ wordstem.engTranslation }}</i></td>
+      <td>{{ wordstem.firstOccurence }}</td>
+      <td>{{ wordstem.semanticField }}</td>
+    </tr>
+    `,
+
+    props: {
+        wordstem: Object
+    },
+
+    created() {
+      setLanguages();
+    },
+}
---
+++ src/components/Wordstems/Wordstems.js
@@ -0,0 +1,119 @@
+import WordstemRow from "./WordstemRow.js";
+import CreateWordstem from "./CreateWordstem.js";
+import { host, pageSize } from "../Config/Config.js";
+export default {
+  components: { WordstemRow, CreateWordstem },
+
+  template: `
+    <section class="showWS">
+    <h2>Lexique etymologique</h2>
+    <h3>Nombre de mots: {{wordStems.length}}</h3>
+    <div>
+        <table class="wstable" id="wstable">
+            <thead style="background-color: rgb(204, 202, 195);">
+                <tr>
+                    <th style="width: 20px;"><a href="#" @click="sortTable(0);"> Lg.</a></th>
+                    <th><a href="#" @click="sortTable(1);"> Mot</a></th>
+                    <th><a href="#" @click="sortTable(2);"> Nature<br>grammaticale</a></th>
+                    <th><a href="#" @click="sortTable(3);"> Traduction</a></th>
+                    <th><a href="#" @click="sortTable(4);"> Année<br>d'occurence</a></th>
+                    <th><a href="#" @click="sortTable(5);"> Champs Lexical</a></th>
+                </tr>
+            </thead>
+            <tbody>
+            <wordstem-row 
+            v-for="wordstem in pageableWordstems"
+            :wordstem="wordstem">
+            </wordstem-row>
+            </tbody>
+        </table>
+        <div id="pagesbutton">
+        <button @click=changePage(pageNum-1)>Précédent</button>
+        <span>Page: {{pageNum}}</span>
+        <button @click=changePage(pageNum+1)>Suivant</button>
+        </div>
+        <button @click="popAddWordstem()>Ajouter un terme</button>
+    </div>
+    <create-wordstem></create-wordstem>
+    </section>
+    `,
+
+  data() {
+    return {
+      wordStems: [],
+      pageNum: 1
+    }
+  },
+
+  created() {
+    fetch(host + '/wordstems', {
+      method: "GET"
+    }
+    )
+      .then(response => response.json())
+      .then(wordStems => {
+        this.wordStems = wordStems;
+      });
+
+    //insertdata(data, pageNum, pageSize);
+  },
+
+  computed: {
+    pageableWordstems() {
+      return this.wordStems.slice((this.pageNum - 1) * pageSize, (this.pageNum) * pageSize)
+    },
+    filters() {
+      return {
+        isBreton: this.wordStems.filter(wordstem => wordstem.wordstemLanguage == "LB")
+      };
+    }
+  },
+
+  methods: {
+    changePage(page) {
+      this.pageNum = page;
+    },
+
+    popAddWordstem() {
+      CreateWordstem.ge
+      modal.style.display = "block";
+      getSources();
+      let sourceElement = document.getElementById("source");
+      let sourcelist = localStorage.getItem("sourcelist");
+      console.log(sourcelist)
+      JSON.parse(sourcelist).forEach((source) => {
+        newOption = document.createElement("option")
+        console.log(source)
+        newOption.value = source.sourceAbbreviation;
+        newOption.innerText = source.sourceOriginalName;
+        sourceElement.appendChild(newOption)
+      })
+    },
+
+    sortTable(columnIndex) {
+      switch (columnIndex) {
+        case 0:
+          this.wordStems.sort((a, b) => (a.wordStemLanguage > b.wordStemLanguage) ? 1 : -1);
+          break;
+        case 1:
+          this.wordStems.sort((a, b) => (a.wordStemName > b.wordStemName) ? 1 : -1);
+          break;
+        case 2:
+          this.wordStems.sort((a, b) => (a.wordClass > b.wordClass) ? 1 : -1);
+          break;
+        case 3:
+          this.wordStems.sort((a, b) => (a.frTranslation > b.frTranslation) ? 1 : -1);
+          break;
+        case 4:
+          this.wordStems.sort((a, b) => (a.firstOccurence > b.firstOccurence) ? 1 : -1);
+          break;
+        case 5:
+          this.wordStems.sort((a, b) => (a.semanticField > b.semanticField) ? 1 : -1);
+          break;
+        default:
+          console.log(`No corresponding column to sort.`);
+      }
+      this.pageNum=1
+    }
+  }
+}
