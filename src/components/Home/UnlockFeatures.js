6a074d80c1ffdaa36db182e19dce2785383da3aa
Kerbetth <kerbetth@protonmail.com>
2024-05-07 16:49:21 +0200

    add vue components for home page

---
+++ src/components/Home/UnlockFeatures.js
@@ -0,0 +1,47 @@
+export default {
+
+    template: `
+    <section class="unlock-features">
+    <h2>Dernières fiches de noms propres débloquées :</h2>
+
+    <div class="last-nouns">
+        <div class="new-noon">
+            <div class="new-noon-img">
+                <img src="../images/paris.jpg" width="150" height="150" border-radius="10" alt="Paris">
+            </div>
+            <div class="new-noon-text">
+                <h3>Paris</h3>
+                <p>
+                    <b>Description:</b> "Capitale des Parisii, initialement construite sur la rive gauche de la Seine,
+                    puis nommée
+                    Lutetia ou Loukotokia lors de l'installation romaine",<br>
+                    <b>Nom gaulois:</b>"Parisios"<br>
+                    <b>Composition::</b>"kario" (le Chaudron)<br>
+                    <b>Traduction Littérale:</b> "La ville des Chaudronniers"<br>
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="last-nouns">
+        <div class="new-noon">
+            <div class="new-noon-img">
+                <a href="../images/rouen.jpg"><img src="../images/rouen.jpg" width="150" height="150" border-radius="10"
+                        alt="Paris"></a>
+            </div>
+            <div class="new-noon-text">
+                <h3>Rouen</h3>
+                <p>
+                    <b>Description:</b> "La ville a été fondée sur la rive droite de la Seine durant le règne
+                    d’Auguste.",<br>
+                    <b>Nom gaulois:</b>"Rotomagos"<br>
+                    <b>Composition::</b>"roto" (roue / course), "mag"(plaine / marché), "os"(suffixe de lieu)<br>
+                    <b>Traduction Littérale:</b> "La PLaine de la Course de Char"<br>
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+    `,
+
+
+}
