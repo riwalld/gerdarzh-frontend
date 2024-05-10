ff0876db52ee3bba68723a7a848954b3880e3844
Kerbetth <kerbetth@protonmail.com>
2024-05-10 22:53:32 +0200

    improve the ui of the config page of the game

--- src/components/Game/ConfigSession.js
+++ src/components/Game/ConfigSession.js
@@ -3,15 +3,16 @@ export default {
     <section class="quiz">
     <h3>Principe du jeu</h3>
     <p>Des noms d'origine celtique vont s'afficher succivement. Vous devrez alors trouver la signification littérale du nom.<br>
-    Les radicaux comosant le nom sont proposés comme aide, vous pouvez voir la traduction correspondante en cliquant dessus. <p>
+    Les radicaux comosant le nom sont proposés comme aide, vous pouvez voir la traduction correspondante en cliquant dessus. </p>
     <p>Bonne chance !</p>
-<h3>Choisissez un thème lexical:</h3>
+
 <div style="padding:30px">
-<button :class=styleBtn id="1" @click="setWordTheme(1)">Lieux et Pays</button>
-<button :class=styleBtn id="2" @click="setWordTheme(2)">Figures Historiques</button>
-<button :class=styleBtn id="3" @click="setWordTheme(3)">Figures Mythiques</button>
-<button :class=styleBtn id="4" @click="setWordTheme(4)">Peuples et Tribus</button>
-<button :class=styleBtn id="5" @click="setWordTheme(5)">Armes et Créatures</button>
+<h3>Choisissez un thème lexical:</h3>
+<button :class=styleBtn class="places" id="1" @click="setWordTheme(1)">Lieux et Pays</button>
+<button :class=styleBtn class="historic" id="2" @click="setWordTheme(2)">Figures Historiques</button>
+<button :class=styleBtn class="mythic" id="3" @click="setWordTheme(3)">Figures Mythiques</button>
+<button :class=styleBtn class="tribes" id="4" @click="setWordTheme(4)">Peuples et Tribus</button>
+<button :class=styleBtn class="weapons" id="5" @click="setWordTheme(5)">Armes et Créatures</button>
 </div>
 <div style="padding:30px">
 <h3 style="color: grey">Choisissez un thème culturelle (à venir)</h3>
@@ -20,30 +21,36 @@ export default {
 <button :class=styleFrozenbtn id="2">Grande et petite Bretagne</button>
 <button :class=styleFrozenbtn id="4">Tous</button>
 </div>
-<button :class=styleBtn id="validate" @click="generateSession()" v-if="wordTheme > 0">Valider</button>
+<button :class="setValidateBtn()" id="validate" @click="generateSession()">Valider</button>
     </section>
     `,
 
     data() {
         return {
             wordTheme: 0,
-            styleSelectedBtn : "btn selected",
-            styleBtn : "btn",
+            styleSelectedBtn : "themeSelected",
+            styleBtn : "themebtn",
             styleFrozenbtn : "btn frozen"
         }
     },
     methods: {
         setWordTheme(wordTheme) {
             this.wordTheme = wordTheme;
-            for (let index = 1; index < 6; index++) {
-                document.getElementById(index).className = this.styleBtn;
+           for (let index = 1; index < 6; index++) {
+                document.getElementById(index).classList.remove(this.styleSelectedBtn);
             }
-            document.getElementById(wordTheme).className = this.styleSelectedBtn;
+            document.getElementById(wordTheme).classList.add(this.styleSelectedBtn);
         },
         
         generateSession() {
 
           this.$emit('generateSession', this.wordTheme);
-        }
+        },
+        setValidateBtn() {
+            if (this.wordTheme == 0) {
+                return "btn validate frozen";
+            }
+            else return "btn validate"
+        },
     }
 }
