7c2ff18c36bd2df17c556362ebc6fa1ed377c632
Benjamin D <57868057+riwalld@users.noreply.github.com>
2025-01-11 02:02:25 +0100

    feat (locale) add i18n config

--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@ import Root from './components/Root.vue'
 import router from './router/index.js'
 import "./main.css";
 
+import { createI18n } from 'vue-i18n'
+import messages from '@intlify/unplugin-vue-i18n/messages'
+
+const i18n = createI18n({
+  locale: 'en',
+  fallbackLocale: ['br','fr'],
+  messages
+});
+
 createApp(Root)
   .use(router)
+  .use(i18n)
   .mount('#app')
