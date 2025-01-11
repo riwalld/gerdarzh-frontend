605949519f3c11a830dca9ee97abd2e6c9fc7f73
Benjamin D <57868057+riwalld@users.noreply.github.com>
2025-01-11 02:05:07 +0100

    feat (ui) convert vuejs to vue3 format for properNounArticle

--- src/components/Propernouns/ProperNounArticle.vue
+++ src/components/Propernouns/ProperNounArticle.vue
@@ -1,24 +1,52 @@
+<script setup>
+import { ref, defineProps, defineEmits, onMounted } from 'vue';
+import { host } from '@/components/Config/Config';
+import { getImage } from '@/utils/utils';
+import { Propernoun } from '@/utils/types';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
+const props = defineProps({
+  currentName: { type: String, required: true }
+});
+
+const emit = defineEmits(['handleShowProperNoun']);
+
+const propernoun = ref<Propernoun | null>(null);
+
+onMounted(() => {
+  fetch(host + '/properNouns/propernoun?current_name=' + props.currentName, {
+    method: 'GET',
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      propernoun.value = data;
+    });
+});
+
+function close() {
+  emit('handleShowProperNoun', null);
+}
+</script>
+
 <template>
   <div id="modal-bg" class="modal">
-
-    <!-- Modal content -->
     <div class="modal-content">
       <span class="close" @click="close()">&times;</span>
         <div style="display: flex; padding: 10px;">
           <div class="etymoInfo">
             <div>
-                  <img :src="getImagePath()" />
+                  <img :src="getImage(propernoun.getImage)" />
                 </div>
                 <div style="display: flex; flex-direction: column;">
                 <div style="padding: 30px;">
                     <h2 style="padding-bottom: 20px;">{{ propernoun.currentName }} </h2>
                     <div style="font-size: larger; font-weight: bold;">{{ propernoun.descrFr }}</div>
                 </div>
-                <div> Sens littéral: {{ propernoun.litTrans.litTransFr }}</div>
-                <div> Forme étymologique: {{ propernoun.etymoName }}</div>
-                <div> Lieu: {{ propernoun.place }}, {{ propernoun.country }}</div>
-                <div> Période: {{ propernoun.period }}</div>
-
+                <div> {{t('literal_meaning')}}: {{ propernoun.litTrans.litTransFr }}</div>
+                <div> {{t('etymological_form')}}: {{ propernoun.etymoName }}</div>
+                <div> {{t('place')}}: {{ propernoun.place }}, {{ propernoun.country }}</div>
+                <div> {{t('period')}}: {{ propernoun.period }}</div>
             </div>
             </div>
           </div>
@@ -26,42 +54,3 @@
   </div>
 
 </template>
-
-<script>
-
-import { host } from '../Config/Config';
-export default {
-
-  props: {
-    currentName :{ type: String, required: true },
-  },
-
-  data() {
-    return {
-      propernoun : { type: Object, required: true },
-    };
-  },
-
-  created() {
-    fetch(host + '/properNouns/propernoun?current_name=' + this.currentName, {
-                method: "GET"
-            }
-            )
-                .then(response => response.json())
-                .then(propernoun => {
-                    this.propernoun = propernoun;
-                });
-
-  },
-  methods:{
-    close() {
-      this.$emit('handleShowProperNoun', null);
-    },
-    getImagePath() {
-      console.log(new URL('../../images/nouns/'+ this.propernoun.image + '.jpg', import.meta.url).href)
-    return new URL(`../../images/nouns/${this.propernoun.image}.jpg`, import.meta.url).href;
-    }
-  }
-}
-
-</script>
