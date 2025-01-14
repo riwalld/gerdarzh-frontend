f1c5c28634beaf20374dd763c6114d0df3f4aa88
Benjamin D <57868057+riwalld@users.noreply.github.com>
2025-01-15 00:01:39 +0100

    feat (ui) adapt locales to ProperNounRow.vue

--- src/components/Propernouns/PropernounRow.vue
+++ src/components/Propernouns/PropernounRow.vue
@@ -1,3 +1,16 @@
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+
+const props = defineProps({
+  propernoun: Object
+});
+
+const emit = defineEmits();
+
+const handleShowProperNoun = (ProperNoun) => {
+  emit('handleShowProperNoun', ProperNoun);
+};
+</script>
 <template>
 
 <tr class="wstr">
@@ -10,45 +23,3 @@
      <td class="cell">{{propernoun.country}}</td>
      <td class="cell"></td></tr>
 </template>
-<script>
-import { setLanguages } from "../Config/Config.js";
-export default {
-    template: `
-    
-    `,
-
-    props: {
-        propernoun: Object
-    },
-
-    created() {
-      setLanguages();
-      //getWordTheme(this.properNoun.wordTheme)
-    },
-
-    methods: {
-      getWordTheme(wordTheme) {
-        switch (wordTheme) {
-          case 1:
-            return "Ville et Pays";
-          case 2:
-            return "Fig. historique";
-          case 3:
-            return "Fig. mythiques";
-          case 4:
-            return "Peuples et Tribus";
-          case 5:
-            return "Armes et Créatures";
-          default:
-            console.log(`No corresponding wortheme for the number.`);
-        }
-      },
-
-    handleShowProperNoun(ProperNoun) {
-      console.log(ProperNoun)
-      this.$emit('handleShowProperNoun', ProperNoun);
-    }
-  }
-    
-}
-</script>
