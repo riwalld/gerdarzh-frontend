29f60c8a215d959e4a0751c073e94c1e3884c05a
Benjamin D <57868057+riwalld@users.noreply.github.com>
2025-01-15 00:02:36 +0100

    feat (ui) adapt locales to WordstemRow.vue

--- src/components/Wordstems/WordstemRow.vue
+++ src/components/Wordstems/WordstemRow.vue
@@ -1,13 +1,37 @@
+<script setup lang='ts'>
+import { defineProps, defineEmits, ref, onMounted } from 'vue';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
+
+
+const props = defineProps({
+  wordstem: Object,
+    semfields: {
+    type: Array as () => Object[], // Typage explicite ici
+    required: true
+  }
+});
+const emit = defineEmits(['handleShowWordstem']);
+
+const handleShowWordstem = (wordstem : Object) => {
+  console.log(wordstem);
+  emit('handleShowWordstem', wordstem);
+};
+
+
+</script>
+
 <template>
-  <tr>
+  <tr v-if="wordstem">
     <td>
-      <span class="langws" :title="wordstem.wordStemLanguage"> {{ wordstem.wordStemLanguage }} </span>
+      <span class="langws" :title="t(wordstem.wordStemLanguage)"> {{ t(wordstem.wordStemLanguage+'_abbr') }} </span>
     </td>
     <td> <b><a class="entry" @click="handleShowWordstem(wordstem)">{{
       wordstem.wordStemName }} </a></b></td>
     <td>
-      <span class="langws" :title="wordstem.wordClass">{{ wordstem.wordClass }}</span>
-      <span class="langws" :title="wordstem.gender">{{ wordstem.gender }}</span>
+      <span class="langws" :title="t(wordstem.wordClass)">{{ t(wordstem.wordClass+'_abbr') }}</span>
+      <span class="langws" :title="t(wordstem.gender)">{{ t(wordstem.gender+'_abbr') }}</span>
     </td>
     <td>
       <span class="langws" title="français">fr.</span>
@@ -16,32 +40,6 @@
       : <i>{{ wordstem.engTranslation }}</i>
     </td>
     <td>{{ wordstem.firstOccurrence }}</td>
-    <td>{{ semfields[wordstem.semanticField-1].frName }}</td>
+    <td v-if="semfields">{{ semfields[wordstem.semanticField-1].frName }}</td>
   </tr>
 </template>
-<script>
-import { setLanguages } from "../Config/Config.js";
-export default {
-
-  props: {
-    wordstem: Object,
-    semfields :Array
-  },
-
-  data() {
-    return {
-      showWordstemArticle : false,
-    }
-  },
-
-  created() {
-    setLanguages();
-  },
-
-  methods:{
-    handleShowWordstem(wordstem) {
-      console.log(wordstem)
-      this.$emit('handleShowWordstem', wordstem);
-    }
-  }
-}</script>
