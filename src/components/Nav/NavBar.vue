e039478b646f37b5c5a7f7a65ec799103bba827d
Benjamin D <57868057+riwalld@users.noreply.github.com>
2025-01-19 22:37:41 +0100

    feat (ui) add language choice on navbar

--- src/components/Nav/NavBar.vue
+++ src/components/Nav/NavBar.vue
@@ -1,12 +1,13 @@
-
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
-import NavLogo from '../../images/battersea_logo.png';
 import { useI18n } from 'vue-i18n';
 
-const { t } = useI18n();
-const navLogo = NavLogo;
 const burger = ref(false);
+const { t, locale } = useI18n();
+
+function changeLanguage(lang: string) {
+  locale.value = lang;
+}
 
 function handleBurgerMenu() {
   burger.value = !burger.value;
@@ -20,32 +21,50 @@ onMounted(() => {
 <template>
   <header style="justify-content:center ;  margin: 0 auto;">
     <div style="display: flex; justify-content:center ; max-width: 1100px; width: 100%;">
-    <!--<img style="top: 30px;" :src="navLogo" width="80" height="80" alt="test">-->
-    <h1 style="color: rgb(51, 51, 51); font-size: 32px;">
-      <a href="/">{{ t('title') }}</a>
-    </h1>
+      <!--<img style="top: 30px;" :src="navLogo" width="80" height="80" alt="test">-->
+      <h1 style="color: rgb(255,230,200); font-size: 32px;">
+        <a href="/">{{ t('title') }}</a>
+      </h1>
 
-    <div class="summary" id="summary-menu">
-      <div class="summary-btn"><a :href="$router.resolve({ name: 'Home' }).href">{{ t('home') }}</a></div>
-      <div class="summary-btn"><a :href="$router.resolve({ name: 'SessionGame' }).href">{{ t('game') }}</a></div>
-      <div class="summary-btn"><a :href="$router.resolve({ name: 'Worsdtems' }).href">{{ t('lexicon') }}</a></div>
-      <div class="summary-btn"><a :href="$router.resolve({ name: 'ProperNouns' }).href">{{ t('entities') }}</a></div>
-      <div class="summary-btn"><a :href="$router.resolve({ name: 'UserProfile' }).href">{{ t('profile') }}</a></div>
-      <div class="language"></div>
-    </div>
-
-    <div class="burger-menu" style="display: none;">
-      <div class="burger-icon" id="burger-icon" @click="handleBurgerMenu">
-        &#9776; <!-- Burger icon -->
-      </div>
-      <div class="burger-list" v-show="burger">
+      <div class="summary" id="summary-menu">
         <div class="summary-btn"><a :href="$router.resolve({ name: 'Home' }).href">{{ t('home') }}</a></div>
         <div class="summary-btn"><a :href="$router.resolve({ name: 'SessionGame' }).href">{{ t('game') }}</a></div>
         <div class="summary-btn"><a :href="$router.resolve({ name: 'Worsdtems' }).href">{{ t('lexicon') }}</a></div>
         <div class="summary-btn"><a :href="$router.resolve({ name: 'ProperNouns' }).href">{{ t('entities') }}</a></div>
         <div class="summary-btn"><a :href="$router.resolve({ name: 'UserProfile' }).href">{{ t('profile') }}</a></div>
-        <div class="language"></div>
+        <div style="display: flex; flex-direction: column;">
+
+          <span @click="changeLanguage('br')" :class="{
+            'bold': locale === 'br',
+          }" class="change-lang">
+            brezhoneg
+          </span>
+          <span @click="changeLanguage('fr')" :class="{
+            'bold': locale === 'fr',
+          }" class="change-lang">
+            français
+          </span>
+          <span @click="changeLanguage('en')" :class="{
+            'bold': locale === 'en',
+          }" class="change-lang">
+            english
+          </span>
+        </div>
       </div>
-    </div></div>
+
+      <div class="burger-menu" style="display: none;">
+        <div class="burger-icon" id="burger-icon" @click="handleBurgerMenu">
+          &#9776; <!-- Burger icon -->
+        </div>
+        <div class="burger-list" v-show="burger">
+          <div class="summary-btn"><a :href="$router.resolve({ name: 'Home' }).href">{{ t('home') }}</a></div>
+          <div class="summary-btn"><a :href="$router.resolve({ name: 'SessionGame' }).href">{{ t('game') }}</a></div>
+          <div class="summary-btn"><a :href="$router.resolve({ name: 'Worsdtems' }).href">{{ t('lexicon') }}</a></div>
+          <div class="summary-btn"><a :href="$router.resolve({ name: 'ProperNouns' }).href">{{ t('entities') }}</a>
+          </div>
+          <div class="summary-btn"><a :href="$router.resolve({ name: 'UserProfile' }).href">{{ t('profile') }}</a></div>
+        </div>
+      </div>
+    </div>
   </header>
 </template>
