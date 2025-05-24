8daa498e4f0afea90fb3d765222cf698404b139f
Benjamin D <57868057+riwalld@users.noreply.github.com>
2025-05-24 09:53:23 +0200

    feat (ui) add wordstem description fields

--- src/components/Wordstems/CreateWordstem.vue
+++ src/components/Wordstems/CreateWordstem.vue
@@ -18,8 +18,8 @@ const wordstemDto = ref<WordStemDto>({
   children: [],
   firstOccurrence: '',
   semanticField: semFieldl.value.id,
-  descrEng: '',
-  descrFr: '',
+  engDescription: '',
+  frDescription: '',
   phonetic: '',
   engTranslation: '',
   frTranslation: '',
@@ -64,75 +64,86 @@ onMounted(async () => {
       <span class="close" @click="close()">&times;</span>
       <h2>{{ t('propose_new_term') }}</h2>
       <form @submit.prevent="postWordstem">
-        <label for="wordStemName">{{ t('term') }}</label>
-        <input type="text" v-model="wordstemDto.wordStemName" required><br><br>
+        <div style="display: flex; flex-direction: row; gap: 40px;">
+          <div>
+            <label for="wordStemName">{{ t('term') }}</label>
+            <input type="text" v-model="wordstemDto.wordStemName" required><br><br>
 
-        <label for="wordStemLanguage">{{ t('language_of_word') }}</label>
-        <select v-model="wordstemDto.wordStemLanguage" required>
-          <option value="LB">{{ t('breton') }}</option>
-          <option value="BBQ">{{ t('brittonic') }}</option>
-          <option value="LC">{{ t('cornish') }}</option>
-          <option value="LE">{{ t('english') }}</option>
-          <option value="LF">{{ t('french') }}</option>
-          <option value="LG">{{ t('gaulish') }}</option>
-          <option value="LGER">{{ t('germanic') }}</option>
-          <option value="LIE">{{ t('proto_indoeuropean') }}</option>
-          <option value="LIR">{{ t('irish') }}</option>
-          <option value="LOI">{{ t('old_irish') }}</option>
-          <option value="LS">{{ t('scottish_gaelic') }}</option>
-          <option value="LPC">{{ t('proto_celtic') }}</option>
-          <option value="LW">{{ t('welsh') }}</option>
-          <option value="LLT">{{ t('latin') }}</option>
-        </select><br><br>
+            <label for="wordStemLanguage">{{ t('language_of_word') }}</label>
+            <select v-model="wordstemDto.wordStemLanguage" required>
+              <option value="LB">{{ t('breton') }}</option>
+              <option value="BBQ">{{ t('brittonic') }}</option>
+              <option value="LC">{{ t('cornish') }}</option>
+              <option value="LE">{{ t('english') }}</option>
+              <option value="LF">{{ t('french') }}</option>
+              <option value="LG">{{ t('gaulish') }}</option>
+              <option value="LGER">{{ t('germanic') }}</option>
+              <option value="LIE">{{ t('proto_indoeuropean') }}</option>
+              <option value="LIR">{{ t('irish') }}</option>
+              <option value="LOI">{{ t('old_irish') }}</option>
+              <option value="LS">{{ t('scottish_gaelic') }}</option>
+              <option value="LPC">{{ t('proto_celtic') }}</option>
+              <option value="LW">{{ t('welsh') }}</option>
+              <option value="LLT">{{ t('latin') }}</option>
+            </select><br><br>
 
-        <label for="phonetic">{{ t('phonetic') }}</label>
-        <input type="text" v-model="wordstemDto.phonetic" required><br><br>
+            <label for="phonetic">{{ t('phonetic') }}</label>
+            <input type="text" v-model="wordstemDto.phonetic" required><br><br>
 
-        <label for="gender">{{ t('gender') }}</label>
-        <select v-model="wordstemDto.gender" required>
-          <option value="GM">{{ t('masculine') }}</option>
-          <option value="GF">{{ t('feminine') }}</option>
-          <option value="GN">{{ t('neutral') }}</option>
-          <option value="GU">{{ t('unknown') }}</option>
-          <option value="NO">{{ t('na') }}</option>
-        </select><br><br>
+            <label for="gender">{{ t('gender') }}</label>
+            <select v-model="wordstemDto.gender" required>
+              <option value="GM">{{ t('masculine') }}</option>
+              <option value="GF">{{ t('feminine') }}</option>
+              <option value="GN">{{ t('neutral') }}</option>
+              <option value="GU">{{ t('unknown') }}</option>
+              <option value="NO">{{ t('na') }}</option>
+            </select><br><br>
 
-        <label for="wordClass">{{ t('word_class') }}</label>
-        <select v-model="wordstemDto.wordClass" required>
-          <option value="WN">{{ t('noun') }}</option>
-          <option value="WV">{{ t('verb') }}</option>
-          <option value="WADJ">{{ t('adjective') }}</option>
-          <option value="WADV">{{ t('adverb') }}</option>
-          <option value="WART">{{ t('article') }}</option>
-          <option value="WPRN">{{ t('pronoun') }}</option>
-          <option value="WPREP">{{ t('preposition') }}</option>
-          <option value="WCONJ">{{ t('conjunction') }}</option>
-          <option value="WAF">{{ t('affix') }}</option>
-        </select><br><br>
+            <label for="wordClass">{{ t('word_class') }}</label>
+            <select v-model="wordstemDto.wordClass" required>
+              <option value="WN">{{ t('noun') }}</option>
+              <option value="WV">{{ t('verb') }}</option>
+              <option value="WADJ">{{ t('adjective') }}</option>
+              <option value="WADV">{{ t('adverb') }}</option>
+              <option value="WART">{{ t('article') }}</option>
+              <option value="WPRN">{{ t('pronoun') }}</option>
+              <option value="WPREP">{{ t('preposition') }}</option>
+              <option value="WCONJ">{{ t('conjunction') }}</option>
+              <option value="WAF">{{ t('affix') }}</option>
+            </select><br><br>
 
-        <label for="engTranslation">{{ t('english_translation') }}</label>
-        <input type="text" v-model="wordstemDto.engTranslation" required><br><br>
+            <label for="engTranslation">{{ t('english_translation') }}</label>
+            <input type="text" v-model="wordstemDto.engTranslation" required><br><br>
 
-        <label for="frTranslation">{{ t('french_translation') }}</label>
-        <input type="text" v-model="wordstemDto.frTranslation" required><br><br>
+            <label for="frTranslation">{{ t('french_translation') }}</label>
+            <input type="text" v-model="wordstemDto.frTranslation" required><br><br>
 
-        <label for="semanticField">{{ t('semantic_field') }}</label>
-        <select v-model="wordstemDto.semanticField" required>
-          <option v-for="semfield in semfields" :key="semfield.id" :value="semfield.id">
-            {{ semfield.frName }}
-          </option>
-        </select><br><br>
+            <label for="engDescription">{{ t('english_Description') }}</label>
+            <input type="text" v-model="wordstemDto.engDescription" required><br><br>
 
-        <label for="firstOccurence">{{ t('first_occurrence') }}</label>
-        <input type="number" v-model="wordstemDto.firstOccurrence" required><br><br>
+            <label for="frDescription">{{ t('french_Description') }}</label>
+            <input type="text" v-model="wordstemDto.frDescription" required><br><br>
+          </div>
+          <div style="width: 200px;">
+            <label for="semanticField">{{ t('semantic_field') }}</label>
+            <select v-model="wordstemDto.semanticField" required>
+              <option v-for="semfield in semfields" :key="semfield.id" :value="semfield.id">
+                {{ semfield.frName }}
+              </option>
+            </select><br><br>
 
-        <label for="sources">{{ t('reference_book') }}</label>
-        <select v-if="sourceList" v-model="wordstemDto.sources" multiple required>
-          <option v-for="source in sourceList" :key="source.sourceId" :value="source.sourceId">{{
-            source.sourceOriginalName }}
-          </option>
-        </select>
-        <button type="submit">Envoyer</button>
+            <label for="firstOccurence">{{ t('first_occurrence') }}</label>
+            <input type="number" v-model="wordstemDto.firstOccurrence" required><br><br>
+
+            <label for="sources">{{ t('reference_book') }}</label>
+            <select v-if="sourceList" v-model="wordstemDto.sources" multiple required>
+              <option v-for="source in sourceList" :key="source.sourceId" :value="source.sourceId">{{
+                source.sourceOriginalName }}
+              </option>
+            </select>
+            <button type="submit">Envoyer</button>
+          </div>
+        </div>
       </form>
     </div>
   </div>
