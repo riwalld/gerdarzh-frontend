ceaa2791dd7aed856bcf6d0946672000d4c7ac7d
Benjamin D <57868057+riwalld@users.noreply.github.com>
2025-05-30 16:15:20 +0200

    fix (ui) from old vue to vue setup

--- src/components/Root.vue
+++ src/components/Root.vue
@@ -1,13 +1,11 @@
+<script setup lang="ts">
+import NavBar from "./Nav/NavBar.vue";
+import FooterBar from "./Nav/FooterBar.vue";
+</script>
+
+
 <template>
     <nav-bar></nav-bar>
     <router-view></router-view>
     <footer-bar></footer-bar>
 </template>
-
-<script>
-import NavBar from "./Nav/NavBar.vue";
-import FooterBar from "./Nav/FooterBar.vue";
-export default {
-    components: { NavBar, FooterBar },
-}
-</script>
