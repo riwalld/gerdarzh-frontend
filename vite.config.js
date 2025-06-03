cd6dc0e7529025857f282a4488022a32180800b8
Benjamin D. <dufeu.benjamin.r@gmail.com>
2025-06-03 21:56:43 +0200

    fix (vite) add allowedHosts

--- vite.config.js
+++ vite.config.js
@@ -20,6 +20,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0', 
     port: 3000, 
+    allowedHosts: ['gerdarzh.eu'],
     hmr: {
       protocol: 'ws', 
       host: 'localhost',
