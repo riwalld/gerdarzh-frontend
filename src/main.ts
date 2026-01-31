19fe1c7271fe589f5b6bb8d49bac468e48c865df
Benjamin D <57868057+riwalld@users.noreply.github.com>
2026-01-31 13:17:49 +0100

    feat (settings) breton as default language

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,21 @@
 import { createApp } from 'vue/dist/vue.esm-bundler'
 import App from './App.vue'
 import router from './router/index.js'
-import "./main.css";
-import en from './locales/en.json';
-import br from './locales/br.json';
-import fr from './locales/fr.json';
+import './main.css'
+import en from './locales/en.json'
+import br from './locales/br.json'
+import fr from './locales/fr.json'
 import { createI18n } from 'vue-i18n'
 import { createPinia } from 'pinia'
 const pinia = createPinia()
 const i18n = createI18n({
-  locale: 'en',
-  fallbackLocale: ['br', 'fr'],
+  locale: 'br',
+  fallbackLocale: ['en', 'fr'],
   messages: {
     en,
     br,
-    fr,
-  },
-});
+    fr
+  }
+})
 
-createApp(App)
-  .use(router)
-  .use(i18n)
-  .use(pinia)
-  .mount('#app')
+createApp(App).use(router).use(i18n).use(pinia).mount('#app')
